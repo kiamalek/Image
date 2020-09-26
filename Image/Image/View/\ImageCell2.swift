047de80103d758fb1783d6e4a55041755f3f86c9
Kia <makhola2@gmail.com>
2020-09-26 21:59:11 +0330

    why

---
+++ Image/Image/View/\ImageCell2.swift
@@ -0,0 +1,24 @@
+//
+//  \ImageCell.swift
+//  Image
+//
+//  Created by Kia Malek on 26.09.20.
+//  Copyright © 2020 Kia Malek. All rights reserved.
+//
+
+import UIKit
+
+class _ImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
